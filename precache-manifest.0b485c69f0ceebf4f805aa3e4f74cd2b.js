c4ca10901b36d4abebe7c688d56b596a5a73c200
unknown <vishalsingh2402@gmail.com>
2019-09-05 19:13:16 +0530

    Updates

---
+++ precache-manifest.0b485c69f0ceebf4f805aa3e4f74cd2b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f372c17059c8296a7853233022f8f53c",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "1bb74c38e4c4d9133653",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "304f1d3a01eca1b7b4b7",
+    "url": "/robofriends/static/css/main.545fd486.chunk.css"
+  },
+  {
+    "revision": "1bb74c38e4c4d9133653",
+    "url": "/robofriends/static/js/2.e93fd954.chunk.js"
+  },
+  {
+    "revision": "304f1d3a01eca1b7b4b7",
+    "url": "/robofriends/static/js/main.82bc9170.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
